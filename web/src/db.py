66e40012db6e6f959ba1a6f5b8cab533e84b598d
nihil <666@nabovarme.dk>
2020-04-13 18:48:33 +0200

    work on sql setup

--- web/src/db.py
+++ web/src/db.py
@@ -20,13 +20,43 @@ async def get_connection():
 async def setup_tables():
     async with get_connection() as conn:
         await conn.execute('''
-            CREATE TABLE messages(
-                id VARCHAR(22),
-                created timestamp,
-                data VARCHAR(15000)
+            create type operation_type as enum('write', 'read');
+        ''')
+        await conn.execute('''
+            CREATE TABLE messages
+            (
+                created_at TIMESTAMP DEFAULT NOW(),
+                hash_id varchar(22),
+                data text,
+                PRIMARY KEY (hash_id, created_at)
             )
         ''')
-
+        await conn.execute('''
+            CREATE FUNCTION delete_old_messages() RETURNS trigger
+                LANGUAGE plpgsql
+                AS $$
+            BEGIN
+                DELETE FROM messages WHERE created_at < NOW() - INTERVAL '1 days';
+                RETURN NULL;
+            END;
+            $$;
+        ''')
+        await conn.execute('''
+            CREATE TRIGGER trigger_delete_old_messages
+            AFTER INSERT ON messages
+            EXECUTE PROCEDURE delete_old_messages();
+        ''')
+        await conn.execute('''
+            CREATE TABLE message_statistics 
+            (
+                created_at TIMESTAMP DEFAULT NOW(),
+                hash_id varchar(22),
+                operation operation_type,
+                data_length int,
+                location json,
+                PRIMARY KEY (hash_id, created_at)
+            )
+        ''')
 
 async def create(id, data):
     db[id] = data.decode("ascii")
